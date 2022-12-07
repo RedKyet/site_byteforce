e7255bd17b02202a94a4cfd85725073855e3805f
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2022-12-07 19:05:06 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read </p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read testttt</p>
